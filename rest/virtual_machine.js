09aa460a5f1726bb29f22ab87163ae40c49f3549
Klim K <kirklimushin@gmail.com>
2022-08-31 09:43:49 +0300

    Updated COmponents, Updated Rest API For Virtual Machines

--- rest/virtual_machine.js
+++ rest/virtual_machine.js
@@ -1,12 +1,65 @@
 import "../../configuration_preparer/preparer.js"
+var BACKEND_APPLICATION_PORT = process.env.BACKEND_APPLICATION_PORT
+var BACKEND_APPLICATION_HOST = process.env.BACKEND_APPLICATION_HOST
 
 
 function InitializeVirtualMachineRestController(Configuration) {
   // Rest Controller, that Initializes New Virtual Machine
+  var APIUrl = new url.URL("http://%s:%s/vm/initialize/" % (
+  BACKEND_APPLICATION_HOST, BACKEND_APPLICATION_PORT))
+  Response, Error= $.ajax({
+    url: APIUrl,
+    method: "POST",
+    data: JSON.stringify({"HardwareConfiguration": Configuration}),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Authorization": $.getCookie("jwt-token"),
+      "Access-Control-Allow-Credentials": "true",
+    },
+    success: function(Response){
+        // Processing Initialization Response
+        if (Response.StatusCode != 201 || Response.StatusCode != 200) {
+          return null, Error(Response.Error)
+        }else{
+          return Response.VmInfo, null // Returns Info About Initialized Instance
+        }
+    },
+    error: function(Error) {
+      // Processing Initialization Error
+      return null, Error(Error)
+    }
+  })
+  return Response, Error
 }
 
-function ApplyVirtualMachineConfigurationRestController(CustomConfiguration) {
+function ApplyVirtualMachineConfigurationRestController(CustomConfiguration, VirtualMachineId) {
   // Rest Controller, that Applying Custom Configuration to the Initialized Virtual Machine
+  var APIUrl = new url.URL("http://%s:%s/vm/apply/configuration/" % (
+  BACKEND_APPLICATION_HOST, BACKEND_APPLICATION_PORT))
+  APIUrl.searchParams.append("VirtualMachineId", VirtualMachineId)
+  Response, Error = $.ajax({
+    url: APIUrl,
+    method: "POST",
+    data: {
+      "CustomizedConfiguration": CustomConfiguration,
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Authorization": $.getCookie("jwt-token"),
+      "Access-Control-Allow-Credentials": "true",
+    },
+    success: function (Response) {
+      if (Response.StatusCode != 200 || Response.StatusCode != 201) {
+        return null, Error(Response.Error)
+      }else{
+        return Response.SshInfo, null
+      }
+    },
+    error: function(Error) {
+      return null, Error(Error)
+    }
+  })
+  return Response, Error
 }
 
 
