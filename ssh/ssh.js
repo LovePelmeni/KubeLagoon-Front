259d145a8972e216130d2619a3750d266b03bc1d
Klim K <kirklimushin@gmail.com>
2022-10-11 13:24:10 +0300

    Updated SSH Rest Controllers, Added Loggers

--- ssh/ssh.js
+++ ssh/ssh.js
@@ -1,5 +1,10 @@
 import * as ssh_rest from "../rest/ssh.js"
 
+let winston = require('winston')
+let SshLogger = winston.createLogger({
+  level: 'ERROR',
+  format: winston.format.json(),
+})
 
 class VirtualMachineSshManager {
   // Manager Class, for managing the SSH of the Virtual Machine Server 
@@ -7,14 +12,18 @@ class VirtualMachineSshManager {
     // Returns the SSH Certificate Content for the Virtual Machine Server 
     let CertificateContent, ContentError = ssh_rest.DownloadSshKeyRestController(
     JwtToken, VirtualMachineId)
-    if (ContentError != null) {console.log("Failed to Download SSH Certificate," + ContentError.Error); return null, ''}
+    if (ContentError != null) {SshLogger.log("Failed to Download SSH Certificate," + ContentError.Error); return null, ''}
     return CertificateContent.CertificateContent, CertificateContent.CertificateFilename
   }
   UpdateSshCertificate(JwtToken, VirtualMachineId) {
     // Updates the SSH Certificate for the Virtual Machine Server 
-    console.log(JwtToken, VirtualMachineId)
+    let Updated, UpdateError = ssh_rest.UpdateSshKeyRestController(
+    JwtToken, VirtualMachineId)
+    if (UpdateError != null) {SshLogger.log(
+    "Failed to Update SSL Certificate for the Virtual Server with Identifier: " 
+    + VirtualMachineId + " " + UpdateError)}
+    return Updated, UpdateError
   }
 }
-
 
 export {VirtualMachineSshManager};
