d3c19b5601dd115fb78e8470b0c3275979377b21
Klim K <kirklimushin@gmail.com>
2022-11-10 14:18:47 +0300

    Nft Payment Form Page Update

--- src/views/NftPayment.vue
+++ src/views/NftPayment.vue
@@ -1,21 +1,63 @@
 <script>
+import * as crypto_blockchain_view from "../../crypto_support/deployer.js";
 import { NftPaymentForm } from "../components/payments/NftPaymentForm.vue";
 import  { mapMutations } from "vuex";
+
 export default {
     components: {NftPaymentForm},
+    data() {
+        return {
+            ActivatePaymentFailedBanner: false
+        }
+    },
     template: `
         <nft-payment-form />
     `,
     name: "NftPaymentView",
     methods: {
         ...mapMutations(["TOGGLE_PAYMENT_PAID", "TOGGLE_PAYMENT_UNPAID"]),
+        ProcessPaymentFailed() {
+            // Processing Payment Failed via Crypto Contract 
+            this.ActivatePaymentFailedBanner = true
+        },
+        ProcessPaymentSuccess() {
+            // Processing Payment Succeess via Crypto Contract 
+            this.$router.push({name: "success_page"})
+        },
         PerformTransaction() {
             // Performing the Blockchain Transaction 
+
+            // * Setting up Payment Session And Deploying the Contract
+            let PaymentSessionCredentials = {}
+            PaymentSessionCredentials.Servers = Bill.Metadata.Servers 
+            PaymentSessionCredentials.Owner = customer
+
+            let BlockchainTransactionManager = new crypto_blockchain_view.DeploymentSmartContractManager(
+                this, PaymentSessionCredentials
+            ) 
+            BlockchainTransactionManager.DeploySmartContract() 
         },
         SubmitForm() {
             // Submitting the Blockchain Transaction Form 
+            this.PerformTransaction()
+        },
+    },
+    computed: {
+    ...mapState(["customer", "Bill", "PaymentSucceeded", "PaymentFailed"]),
+    },
+    watch: {
+        PaymentSucceeded: function() {
+            // Checking for the Payment Succeeded Event 
+            if (this.PaymentSucceeded === true) {
+                this.ProcessPaymentSuccess() 
+            }
+        },
+        PaymentFailed: function() {
+            // Checking for the Payment Failed Event
+            if (this.PaymentFailed === true) {
+                this.ProcessPaymentFailed()
+            }
         }
     }
 }
-
 </script>
